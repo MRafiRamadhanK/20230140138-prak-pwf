b52c1521e73703be3e63eda77ca663f0c94b345f
rafirmdhn <raficool271003@gmail.com>
2026-03-06 10:34:35 +0700

    pertemuan 3 bug fixing

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Suppliers List') }}
+            {{ __('Suppliers') }}
         </h2>
     </x-slot>
 
@@ -9,32 +9,34 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <table class="w-full text-left table-auto border-collapse">
-                        <thead>
-                            <tr class="border-b border-gray-600">
-                                <th class="py-2 px-4">ID</th>
-                                <th class="py-2 px-4">Name</th>
-                                <th class="py-2 px-4">Contact</th>
-                                <th class="py-2 px-4">Address</th>
-                                <th class="py-2 px-4">Created At</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($suppliers as $supplier)
-                                <tr class="border-b border-gray-700">
-                                    <td class="py-2 px-4">{{ $supplier->id }}</td>
-                                    <td class="py-2 px-4">{{ $supplier->name }}</td>
-                                    <td class="py-2 px-4">{{ $supplier->contact }}</td>
-                                    <td class="py-2 px-4">{{ $supplier->address }}</td>
-                                    <td class="py-2 px-4">{{ $supplier->created_at->format('d M Y') }}</td>
-                                </tr>
-                            @empty
+                    <div class="overflow-x-auto">
+                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                            <thead class="bg-gray-50 dark:bg-gray-700">
                                 <tr>
-                                    <td colspan="5" class="py-4 text-center">No suppliers available.</td>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">ID</th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Name</th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Contact</th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Address</th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">Created At</th>
                                 </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                @forelse($suppliers as $supplier)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ $supplier->id }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">{{ $supplier->name }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ $supplier->contact }}</td>
+                                        <td class="px-6 py-4 text-sm text-gray-500 dark:text-gray-400">{{ $supplier->address }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-400">{{ $supplier->created_at->format('Y-m-d') }}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5" class="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500 dark:text-gray-400">No suppliers available.</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
