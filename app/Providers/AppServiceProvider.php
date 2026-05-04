80f0ff63127ba51e82bb4a1437af0d62615618b3
rafirmdhn <raficool271003@gmail.com>
2026-05-04 23:08:18 +0700

    pertemuan 9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -27,5 +31,14 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('manage-categories', function (User $user) {
             return $user->role === 'admin';
         });
+
+        Gate::define('viewApiDocs', function (?User $user) {
+            return true;
+        });
+
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            });
     }
 }
